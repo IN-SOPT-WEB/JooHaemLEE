c3365c6d2b0ee09d3fb06c480928d27d0ff41a9f
SNUPI <joohaem@daum.net>
2022-09-26 21:37:46 +0900

    [ feat ] follow-web :: 로직 구현

--- follow-github/src/App.tsx
+++ follow-github/src/App.tsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useRef } from 'react';
+import './App.css';
+import patNotice1 from './assets/1.png';
+import patNotice2 from './assets/2.png';
+
+const ids = [
+  'yoojs1205',
+  'leeseooo',
+  'seojisoosoo',
+  'talkingOrange',
+  'borimong',
+  'NaveOWO',
+  'namjunkim12',
+  'iamphj3',
+  'Seong-Gyeong',
+  'Brokyeom',
+  'seoyeun0106',
+  'Happhee',
+  'hayounSong',
+  'ilmerry',
+  'R1mmm',
+  'Dangpy',
+  'wooyoung0803',
+  'seobbang',
+  'Dev-jwJeong',
+  'choichoijin',
+  'myeongheonhong',
+  'henization',
+  'wook_did',
+];
+
+const provideHeaders = (token: string) => {
+  return {
+    'Content-Type': 'application/json',
+    Accept: 'application/vnd.github.v3+json',
+    Authorization: `Token ${token}`,
+  };
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const patRef = useRef<HTMLInputElement | null>(null);
+
+  const followWebPart = async () => {
+    await Promise.all(
+      ids.map((id) => {
+        if (!patRef || !patRef.current) return;
+
+        return fetch(`https://api.github.com/user/following/${id}`, {
+          method: 'PUT',
+          body: null,
+          headers: provideHeaders(patRef.current.value),
+        });
+      }),
+    );
+
+    alert('팔로우 완료!');
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main>
+      <article>
+        <h2>PAT란, Personal Access Token.</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          깃허브에서 SETTING - Developer Setting - Personal Access Token에 들어가 Token을
+          발급받아요.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+        <p>권한은 user (Update ALL user data)에만 체크해주시면 됩니다.</p>
+        <img src={patNotice1} alt="pat-img1" />
+        <img src={patNotice2} alt="pat-img2" />
+      </article>
+      <a href="https://github.com/settings/tokens" target="_blank">
+        PAT 발급받으러가기
+      </a>
+      <input ref={patRef} type="text" placeholder="PAT를 입력해주세요!" />
+      <button type="button" onClick={followWebPart}>
+        버튼 하나로 웹파트 팔로우하기 ? 정말 ?
+      </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
